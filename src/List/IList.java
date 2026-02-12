f03b24b7c71f2e71c9cebf15a5f3c19887d5b6a7
JuniorLancerio <juniorlancerio77@gmail.com>
2026-02-12 12:36:16 -0600

    Interfaz de Lista corregida.

--- src/List/IList.java
+++
@@ -1,9 +0,0 @@
-package List;
-
-public interface IList<T> {
-    void add(T element);
-    void remove(T element);
-    T get(int index);
-    int size();
-    boolean contains(T element);
-}
--- src/List/Lista.java
+++ src/List/Lista.java
@@ -1,5 +1,16 @@
 package List;
+public interface Lista<T> {
 
-public class Lista {
-    
+    void add(T valor);
+
+    void add(int index, T valor);
+
+    T get(int index);
+
+    T remove(int index);
+
+    int size();
+
+    boolean isEmpty();
+
 }
